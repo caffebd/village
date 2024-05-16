3e1d6f180335e0cb7d2cf5d04eae7b9c68bca76c
caffebd <caffebd@gmail.com>
2024-05-16 13:28:42 +0100

    first commit

---
+++ autoload/global_signals.gd
@@ -0,0 +1,20 @@
+extends Node
+
+signal dad_to_mound()
+signal dad_to_clearing()
+
+signal start_clearing()
+
+signal  stick_drop()
+
+signal time_transition_0
+signal time_transition_1
+signal time_transition_2
+signal time_transition_3
+signal time_transition_4
+signal time_transition_5
+signal time_transition_6
+signal time_transition_7
+signal time_transition_8
+signal time_transition_9
+
---
+++ luke/scripts_luke/hud.gd
@@ -0,0 +1,22 @@
+extends Control
+@onready var target: TextureRect = %target
+
+var target_mode = "off"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	%FPS.text = "FPS " + str(Engine.get_frames_per_second())
+
+func set_target_mode(mode:String):
+	if mode == target_mode:
+		return
+	target_mode = mode
+	if target_mode=="off":
+		target.modulate.a = 145.0
+	elif target_mode == "interact":
+		target.modulate.a = 255.0
