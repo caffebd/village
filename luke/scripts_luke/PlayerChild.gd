dd7b4b0eaf2731cd08e73303f4d9c90ea55244ff
caffebd <caffebd@gmail.com>
2024-05-16 16:11:50 +0100

    updated world

--- luke/scripts_luke/PlayerChild.gd
+++ luke/scripts_luke/PlayerChild.gd
@@ -125,13 +125,12 @@ func _physics_process(delta):
 	if Input.is_action_pressed("use"):
 		var collider = ray.get_collider()
 		if collider != null:
-			print (collider.name)
 
 			if collider.get_parent().is_in_group("log"):
-				print ("is log")
 				var object = collider.get_parent()
-				object.global_position.z = attack_marker.global_position.z
-				object.global_position.x = attack_marker.global_position.x
+				object.dragged(attack_marker)
+				#object.global_position.z = attack_marker.global_position.z
+				#object.global_position.x = attack_marker.global_position.x
 	
 	var input_dir = Input.get_vector("left", "right", "forward", "back")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
@@ -180,8 +179,9 @@ func _hud_target():
 		hud.target.modulate = Color(1,1,1,0.2)	
 func set_held_object(body):
 	if body is RigidBody3D:
-		heldObject = body
-		heldObject.held = true
+		if body.is_in_group("pick_item"):
+			heldObject = body
+			heldObject.held = true
 	
 func drop_held_object():
 	heldObject = null
--- luke/scripts_luke/log.gd
+++ luke/scripts_luke/log.gd
@@ -1,9 +1,21 @@
 extends Node3D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var colliding: bool = false
 
 
+
+func dragged(drag_marker):
+	if not colliding:
+		global_position.z = drag_marker.global_position.z
+		global_position.x = drag_marker.global_position.x
+
+
+
+
+func _on_drop_area_body_entered(body: Node3D) -> void:
+	colliding = true
+	var tween = create_tween()
+	tween.tween_property(self, "global_position:z", -192.959, 0.5)
+	await tween.finished
+	colliding = false
 
